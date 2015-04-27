1a54b0e3e9736f702ff6df400f2cbbe86e492a9c
sandro <sandro.sole@gmail.com>
2015-04-27 09:06:43 +0200

    filter anstelle functionen, bidirectionales modell anstelle selbst das modell zu stellen

--- app/conf/conf.controller.ts
+++ app/conf/conf.controller.ts
@@ -19,11 +19,7 @@ module app {
         dropElem: IComponent
         dropSignal: string
 
-        onOver: Function
-        onOut: Function
         onDrop: Function
-        startCallback: Function
-        stopCallback: Function
 
         deviceList: Array<IComponent>[]
     }
@@ -32,7 +28,15 @@ module app {
         /* @ngInject */
         static $inject = ["$scope", "Restangular"];
 
+        scope:IConfScope;
+        myService:restangular.IService;
+
         constructor($scope:IConfScope, myService:restangular.IService) {
+            this.scope = $scope;
+            this.myService = myService;
+
+            $scope.dropElem = null;
+
             var basedevices = myService.all('device/devices');
 
             basedevices
@@ -41,52 +45,24 @@ module app {
                     $scope.deviceList = components;
                 });
 
+            $scope.onDrop = () => this.myOnDrop();
 
-            $scope.dropElem = null;
+        }
 
+        private myOnDrop() {
+            console.log('(inside)onDrop');
+            this.scope.dropSignal = 'alert ';
+            console.log(this.scope.dropElem.serialNo);
 
-            $scope.onOver = function (/* event, ui */) {
-                console.log('onOver');
-                $scope.dropSignal = 'alert alert-success show';
+            this.myService
+                .one('device', this.scope.dropElem.serialNo)
+                .post('basicRegister', this.scope.dropElem)
+                .then((components:Array<IComponent>[]) => {
+                    this.scope.deviceList = components;
+                });
 
-                $scope.$apply();
-            };
+            this.scope.$apply();
 
-            $scope.onOut = function (/* event, ui */) {
-                console.log('onOut');
-                $scope.dropSignal = 'alert alert-warning show';
-
-                $scope.$apply();
-            };
-
-            $scope.onDrop = function (/* event, ui */) {
-                console.log('onDrop');
-                $scope.dropSignal = 'alert ';
-                console.log($scope.dropElem.serialNo);
-
-                myService
-                    .one('device', $scope.dropElem.serialNo)
-                    .post('basicRegister', $scope.dropElem)
-                    .then((components:Array<IComponent>[]) => {
-                        $scope.deviceList = components;
-                    });
-
-                $scope.$apply();
-            };
-
-            $scope.startCallback = function (event, ui, dragElem) {
-                console.log('Start dragging: ', dragElem.name);
-                $scope.dropSignal = 'alert alert-warning show';
-
-                $scope.$apply();
-            };
-
-            $scope.stopCallback = function (event, ui, dragElem) {
-                console.log('Stop dragging: ', dragElem.name);
-                $scope.dropSignal = 'alert alert-info ';
-
-                $scope.$apply();
-            };
 
         }
     }
